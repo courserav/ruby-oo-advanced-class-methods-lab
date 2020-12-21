e0db86d22b769a8e83d5f4efe9eb5e0060edc7e7
courserav <lvsmlit@gmail.com>
2020-12-20 19:23:47 -0500

    commit tree ccb90889783a59e4820e4eb903973edc6216d87f

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,81 @@ class Song
   @@all = []
 
   def self.all
+
     @@all
   end
 
   def save
+
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+
+    self.all << song
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+
+    song.name = name
+    self.all << song
+    return song
+  end
+
+  def self.find_by_name(name)
+
+    self.all.find do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+
+    if self.find_by_name(name) != nil
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+
+    self.all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(name)
+
+    song_array = name.split("-")
+    artist_name = song_array[0].strip
+    song_name = song_array[1].split(".")
+    song_name = song_name[0].strip
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    return song
+  end
+
+  def self.create_from_filename(name)
+
+    self.all << self.new_from_filename(name)
+  end
+
+  def self.destroy_all
+
+    self.all.clear
   end
 
 end
